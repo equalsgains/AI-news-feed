e73c346fa7d8d0375337987946b1a2f7b67f6af3
equalsgains <equalsgains@gmail.com>
2020-08-19 01:29:55 -0600

    implemented alanBTN and created intent with what can this app do

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = "";
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ( { command } ) => {
+                if (command === "testCommand"){
+                    alert("this was executed");
+                }
+            } 
+        });
+    }, []);
+
     return(
         <div>
             <h1>Alan AI News Feed</h1>
