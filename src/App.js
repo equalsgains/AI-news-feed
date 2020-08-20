662d3e24eaf514d5b1f5892158d2380776586447
equalsgains <equalsgains@gmail.com>
2020-08-19 19:04:40 -0600

    api setpoints configured, parsed boyd from articles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 
 require('dotenv').config();
 
-const alanKey = ""
+const alanKey = "385a9608d1538ff30eb66423884de8472e956eca572e1d8b807a3e2338fdd0dc/stage"
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ( { command } ) => {
-                if (command === "testCommand"){
-                    alert("this was executed");
+            onCommand: ( { command, articles } ) => {
+                if (command === "headlines"){
+                    setNewsArticles(articles);
                 }
             } 
         });
